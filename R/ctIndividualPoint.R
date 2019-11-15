ce0b3a1929e75d44657289443433551945de547d
Dwight Wynne <dpwynne@gmail.com>
2019-11-15 12:58:06 -0800

    changed t to t0 to reflect that it is individual time point

--- R/ctIndividualPoint.R
+++ R/ctIndividualPoint.R
@@ -1,21 +1,22 @@
 #' c(t) at Individual Points
 #'
-#' Computes the estimated piecewise constant intensity function c(t) at a particular time point t
+#' Computes the estimated piecewise constant intensity function c(t) at a particular time point t0
 #'
-#' @param t a single time value
+#' @param t0 a single time value
 #' @param terminal.points a numeric vector containing the time points at which c(t) changes
 #' @param ct a numeric vector containing the estimated piecewise constant intensity function c(t)
 #'
 #' @return a single numeric value of c(t)
 #'
 #' @export
-ct.individual.point<-function(t,terminal.points,ct){
-	##if t is outside the time frame specified by t.start and t.end, c(t) is 0
-	if( t<min(terminal.points) | t>max(terminal.points) ) return(0)
-	##if t is t.start or t.end, c(t) is the appropriate value
-	if (t==min(terminal.points)) return( ct[1] )
-	if (t==max(terminal.points)) return( ct[length(ct)] )
-	##otherwise find the terminal point immediately before t
-	ct.indx <- max(which(terminal.points <= t))
+
+ct.individual.point<-function(t0, terminal.points, ct){
+	##if t0 is outside the time frame specified by t.start and t.end, c(t0) is 0
+	if( t0 < min(terminal.points) | t0 > max(terminal.points) ) return(0)
+	##if t0 is t.start or t.end, c(t0) is the appropriate value
+	if (t0 == min(terminal.points)) return( ct[1] )
+	if (t0 == max(terminal.points)) return( ct[length(ct)] )
+	##otherwise find the terminal point immediately before t0
+	ct.indx <- max(which(terminal.points <= t0))
 	return(ct[ct.indx])
 }
