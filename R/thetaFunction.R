fa802f1371f6d8cc86a61a140b41f5920b55b285
Dwight Wynne <dpwynne@gmail.com>
2019-11-22 10:36:52 -0800

    fixed documentation re: t and t.start

--- R/thetaFunction.R
+++ R/thetaFunction.R
@@ -2,7 +2,7 @@
 #'
 #' Calculate the value of theta(t) at an individual time point t
 #'
-#' @param t the time at which to calculate \theta(t)
+#' @param t the time at which to calculate \theta(t). The function \code{q.vector} integrates this function, so t can be of any length (including 1).
 #' @param theta a numeric vector containing the estimated intensity function \theta(t).
 #' @param t.start the starting time of the recording
 #' @param resolution the time difference between successive points in the theta vector
@@ -11,5 +11,5 @@
 #'
 #' @export
 
-theta.function<-function(t,theta,t.start,resolution) theta[(1+floor((t-t.start)/resolution))]
+theta.function<-function(t,theta,t.start = 0,resolution) theta[(1+floor((t-t.start)/resolution))]
 ##pull the value of the intensity function that corresponds to a particular time value
