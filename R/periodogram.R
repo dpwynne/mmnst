be2a4072141394a136887a34570b7173e3ee90e7
Reza Ramezan <Reza@v1040-wn-rt-c-127-13.campus-dynamic.uwaterloo.ca>
2020-01-17 14:45:22 -0500

    minor comment added

--- R/periodogram.R
+++ R/periodogram.R
@@ -1,5 +1,6 @@
 # It is important that the starting point of the widow to be 0 and the end point to be T
 # in short, the data points tj must be smaller than T. Otherwise shif the data so that tj<T
+# This needs documentation
 
 periodogram<-function(data, f , T.data , centering = FALSE){ ## f is the frequency vector where the periodogram is evaluated.
   if(!centering){
