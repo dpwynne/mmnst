401a477449a3074bfaf124a75ab06ad2535aaa65
dpwynne <dpwynne@gmail.com>
2022-08-08 12:24:45 -0700

    fix formatting of messages to user

--- R/SelectTopFrequencies.R
+++ R/SelectTopFrequencies.R
@@ -18,8 +18,7 @@ SelectTopFrequencies <- function(f.sorted,K, user.select = FALSE){
 ##in the sorted f.common table
 cat("Identifying",K,"Most Common Peak Frequencies\n")
 f.hat <- head(f.sorted,K)
-cat(f.hat) ##prints the top frequencies and the number of trials they exist at
-cat("\n")
+cat(names(f.hat), "\n", f.hat, "\n")
 top.freqs<-as.numeric(names(f.hat))
 
 if (user.select){  # if user.select, we want user to manually check and accept
@@ -37,6 +36,6 @@ cat("Response Not Accepted\n")
 }  # end while loop
 }
 
-cat(K,"Most Common Peak Frequencies Identified\n")
+#cat(K,"Most Common Peak Frequencies Identified\n")
 return(top.freqs)
 }
