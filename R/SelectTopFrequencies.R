15b5c187a7181252fdb5c460c953c939709d44df
dpwynne <dpwynne@gmail.com>
2022-08-08 12:31:39 -0700

    fix messages to user

--- R/SelectTopFrequencies.R
+++ R/SelectTopFrequencies.R
@@ -18,7 +18,11 @@ SelectTopFrequencies <- function(f.sorted,K, user.select = FALSE){
 ##in the sorted f.common table
 cat("Identifying",K,"Most Common Peak Frequencies\n")
 f.hat <- head(f.sorted,K)
-cat(names(f.hat), "\n", f.hat, "\n")
+
+for(i in 1:K){
+  cat(paste(names(f.hat)[i], "Hz: ", f.hat[i], "trains"), "\n")
+}
+
 top.freqs<-as.numeric(names(f.hat))
 
 if (user.select){  # if user.select, we want user to manually check and accept
