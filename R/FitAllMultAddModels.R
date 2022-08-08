309e3a1acaa0cedc519b66b20191b92c33ddbadd
dpwynne <dpwynne@gmail.com>
2022-08-08 11:28:11 -0700

    replace user.select with user.defined.frequencies to allow user to manually add frequencies that "must" be in the model

--- R/FitAllMultAddModels.R
+++ R/FitAllMultAddModels.R
@@ -8,7 +8,8 @@
 #' @param setup.pars a list of additional parameters for the likelihood function, computed by \code{\link{SetupLikelihoods}}.
 #' @param terminal.points a numeric vector containing the time points at which \eqn{c(t)} changes.
 #' @param ct a numeric vector containing the estimated piecewise constant intensity function \eqn{c(t)}. The length of \eqn{c(t)} should be a whole number power of 2.
-#' @param user.select whether to allow the user to select the frequencies for the model; must be FALSE for this function to run effectively.
+#' @param user.defined.frequencies (= NULL) a numeric vector of length at least K containing the frequencies (in decreasing order of importance) to try in the model.
+#' By default this is NULL, and frequencies will be directly read from f.common.table.
 #'
 #' @return A list of length 3 is returned.
 #' The first item in the list is a list of frequency estimates for each model.
@@ -17,17 +18,22 @@
 #'
 #' @export
 
-FitAllMultAddModels <- function(K, spikes, f.common.table, setup.pars, terminal.points, ct, user.select = FALSE) {
+FitAllMultAddModels <- function(K, spikes, f.common.table, setup.pars, terminal.points, ct, user.defined.frequencies = NULL) {
 ## This abstracts Step 6 and 7 of the original script to run for each neuron
 ## For some value K, it fits Multiplicative 1-K, Additive 1-K, and the no periodicity model (i.e. c(t))
-
 
  f.hat.list <- vector("list", length = 2*K+1)  # length 2K+1 list of NULLs
  w0.hat.list <- vector("list", length = 2*K+1)
  K.list <- vector("list", length = 2*K+1)
 
+ user.top.frequencies <- unique(c(
+   user.defined.frequencies,
+   SelectTopFrequencies(f.common.table, K)
+ ))
+
 for(k in 1:K){
-   f.hat.list[[2*k]] <- f.hat.list[[2*(k-1)+1]] <- SelectTopFrequencies(f.common.table, k, user.select = user.select)
+
+   f.hat.list[[2*k]] <- f.hat.list[[2*(k-1)+1]] <- user.top.frequencies[1:k]
    w0.hat.list[[2*(k-1)+1]] <-  w0.hat.list[[2*k]] <- EstimatePhase(spikes,f.hat.list[[2*k]])
    K.list[[2*(k-1)+1]] <- FitMultiplicativeModel(spikes, f.hat.list[[2*(k-1)+1]], w0.hat.list[[2*(k-1)+1]], setup.pars, terminal.points, ct)
    K.list[[2*k]] <- FitAdditiveModel(spikes, f.hat.list[[2*k]], w0.hat.list[[2*k]], setup.pars, terminal.points, ct)
@@ -35,10 +41,10 @@ for(k in 1:K){
 }
 
    f.hat.list[[2*K+1]] <- 0
-   w0.hat.list[[2*K+1]] <-  EstimatePhase(spikes,f.hat.list[[2*K+1]])
+   w0.hat.list[[2*K+1]] <- 0
    K.list[[2*K+1]] <- FitNonperiodicModel(spikes, setup.pars, terminal.points, ct)
 
-   names(f.hat.list)[2*K+1] <- "Nonperiodic"
+   names(f.hat.list)[2*K+1] <- "Non-Periodic"
    names(w0.hat.list) <- names(f.hat.list)
    names(K.list) <- names(f.hat.list)
 
