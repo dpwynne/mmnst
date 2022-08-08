e65f4165f4bbe9c879985c35cb0d7c78bf11a0d7
dpwynne <dpwynne@gmail.com>
2022-08-08 11:27:40 -0700

    add comments to EstimatePhase for developer consistency between w.hat and w0.hat

--- R/EstimatePhase.R
+++ R/EstimatePhase.R
@@ -16,21 +16,27 @@ EstimatePhase <- function(spikes,f.hat){
 cat("Estimating Phase\n")
 
 w.hat<-2*pi*f.hat
+# w.hat is the angular frequency
+
 ntrials<-length(spikes)
+
 w0.hat <- matrix(NA,ntrials,length(w.hat))
+# w0.hat is a matrix where 1 row = 1 spike train and 1 column = 1 frequency
+# w0.hat contains the phase offsets for each trial for each frequency
 
 for(itr in 1:ntrials){
 	w0.hat.itr <- numeric(length(w.hat))
-	for(i in 1:length(w.hat)){
 	spike.train <- spikes[[itr]]  # this goes outside the second for loop
-	sum.sin<- sum(sin(w.hat[i]*spike.train))
-	sum.cos<-sum(cos(w.hat[i]*spike.train))
-	phi.hat<-atan(-sum.sin/sum.cos)
-	if( sin(phi.hat)*sum.sin > cos(phi.hat)*sum.cos ) phi.hat<-phi.hat+pi
-	w0.hat.itr[i] <- phi.hat/(2*pi)
-	}
+	  for(i in 1:length(w.hat)){
+	    sum.sin<- sum(sin(w.hat[i]*spike.train))
+	    sum.cos<-sum(cos(w.hat[i]*spike.train))
+	    phi.hat<-atan(-sum.sin/sum.cos)
+	    if( sin(phi.hat)*sum.sin > cos(phi.hat)*sum.cos ) phi.hat<-phi.hat+pi
+	    w0.hat.itr[i] <- phi.hat/(2*pi)
+    }
 	w0.hat[itr,] <- w0.hat.itr
 }
+
 cat("Phase Estimated\n")
 return(w0.hat)
 }
