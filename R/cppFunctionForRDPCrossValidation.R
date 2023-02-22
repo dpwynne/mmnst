b42e113a7b04155258eabbc6d617c2a3b557b80b
rramezan <reza@Reza-Mac-Pro-20.local>
2023-02-22 11:09:34 -0500

    cpp Funciton turned into comment so that it does not break the package for now. Vingnette is updated so that it runs. It is jsut reading the data in.

--- R/cppFunctionForRDPCrossValidation.R
+++ R/cppFunctionForRDPCrossValidation.R
@@ -6,7 +6,24 @@
 # Assumes input x is sorted in ascending order so that the inner loop can start
 # searching from the previous index. Another sub-OoM improvement on top and
 # should scale better for large v.
-cppFunction("IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
+
+# cppFunction("IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
+#   IntegerVector y(x.length());
+#   int last_index = 0;
+#   for (unsigned int i = 0; i < x.length(); i++) {
+#     y[i] = 0;
+#     for (unsigned int j = last_index; j < v.length(); j++) {
+#       if (v[j] > x[i]) {
+#         last_index = j;
+#         y[i] = j;
+#         break;
+#       }
+#     }
+#   }
+#   return y;
+# }", depends = "Rcpp")
+
+FFIOX = "IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
   IntegerVector y(x.length());
   int last_index = 0;
   for (unsigned int i = 0; i < x.length(); i++) {
@@ -20,4 +37,8 @@ cppFunction("IntegerVector fast_first_index_vec_ordered_x (NumericVector x, Nume
     }
   }
   return y;
-}", depends = "Rcpp")
+}"
+
+# Rcpp::cppFunction(FFIOX)
+
+
