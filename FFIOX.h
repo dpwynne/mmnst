248699b1342e1d646ba93614022b57717219798e
dpwynne <dpwynne@gmail.com>
2023-02-22 09:01:43 -0800

    This needs to go in the /src folder

---
+++ FFIOX.h
@@ -0,0 +1,15 @@
+IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
+  IntegerVector y(x.length());
+  int last_index = 0;
+  for (unsigned int i = 0; i < x.length(); i++) {
+    y[i] = 0;
+    for (unsigned int j = last_index; j < v.length(); j++) {
+      if (v[j] > x[i]) {
+        last_index = j;
+        y[i] = j;
+        break;
+      }
+    }
+  }
+  return y;
+}
