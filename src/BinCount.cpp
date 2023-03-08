421591dd0e7b91e27cf9bffea1df331742e8841d
dpwynne <dpwynne@gmail.com>
2023-03-08 09:18:39 -0800

    trying R_xlen_t

--- src/BinCount.cpp
+++ src/BinCount.cpp
@@ -3,8 +3,8 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 IntegerVector Cpp_BinCount_sorted(NumericVector x, NumericVector s) {
     IntegerVector counts(s.length() - 1);
-     for (int binnum = 0; binnum < s.length(); binnum++){
-        for (int pos = 0; pos < x.length(); pos++){
+    for (R_xlen_t binnum = 0; binnum < s.length(); binnum++){
+        for (R_xlen_t pos; pos < x.length(); pos++){
             if (x[pos] >= s[binnum + 1]) break;
             counts[binnum]++;
         }
--- src/FFIOX.cpp
+++ src/FFIOX.cpp
@@ -3,10 +3,10 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
   IntegerVector y(x.length());
-  int last_index = 0;
-  for (int i = 0; i < x.length(); i++) {
+  R_xlen_t last_index = 0;
+  for (R_xlen_t i = 0; i < x.length(); i++) {
     y[i] = 0;
-    for (int j = last_index; j < v.length(); j++) {
+    for (R_xlen_t j = last_index; j < v.length(); j++) {
       if (v[j] > x[i]) {
         last_index = j;
         y[i] = j;
