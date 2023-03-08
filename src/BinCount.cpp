d994d127a6374aeb9e6415bea43d4cf72da4b518
dpwynne <dpwynne@gmail.com>
2023-03-08 09:09:20 -0800

    NumericVector is signed int

--- src/BinCount.cpp
+++ src/BinCount.cpp
@@ -3,9 +3,8 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 IntegerVector Cpp_BinCount_sorted(NumericVector x, NumericVector s) {
     IntegerVector counts(s.length() - 1);
-    unsigned int pos = 0;
-    for (unsigned int binnum = 0; binnum < s.length(); binnum++){
-        for (pos; pos < x.length(); pos++){
+     for (int binnum = 0; binnum < s.length(); binnum++){
+        for (int pos = 0; pos < x.length(); pos++){
             if (x[pos] >= s[binnum + 1]) break;
             counts[binnum]++;
         }
