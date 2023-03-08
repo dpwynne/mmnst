d4bf68629234ced8d3ffb6109aefff3a68f89467
rramezan <reza@Reza-Mac-Pro-20.local>
2023-03-08 11:14:49 -0500

    a few white spaces removed from BinCount.cpp All the other files were made during the build

--- src/BinCount.cpp
+++ src/BinCount.cpp
@@ -4,9 +4,10 @@ using namespace Rcpp;
 IntegerVector Cpp_BinCount_sorted(NumericVector x, NumericVector s) {
     IntegerVector counts(s.length() - 1);
     unsigned int pos = 0;
-    for (unsigned int binnum = 0; binnum < s.length(); binnum++) {    for (pos; pos < x.length(); pos++) {
-             if (x[pos] >= s[binnum + 1]) break;      counts[binnum]++;
+    for (unsigned int binnum = 0; binnum < s.length(); binnum++){
+        for (pos; pos < x.length(); pos++){
+            if (x[pos] >= s[binnum + 1]) break;
+            counts[binnum]++;
         }
     }  return counts;
-    
 }
