55d8604bd0557fea37adb2d16a9eb45c3d12ea93
dpwynne <dpwynne@gmail.com>
2023-03-08 09:10:51 -0800

    Numeric vector is signed int

--- src/FFIOX.cpp
+++ src/FFIOX.cpp
@@ -4,9 +4,9 @@ using namespace Rcpp;
 IntegerVector fast_first_index_vec_ordered_x (NumericVector x, NumericVector v) {
   IntegerVector y(x.length());
   int last_index = 0;
-  for (unsigned int i = 0; i < x.length(); i++) {
+  for (int i = 0; i < x.length(); i++) {
     y[i] = 0;
-    for (unsigned int j = last_index; j < v.length(); j++) {
+    for (int j = last_index; j < v.length(); j++) {
       if (v[j] > x[i]) {
         last_index = j;
         y[i] = j;
