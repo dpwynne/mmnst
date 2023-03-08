2641e3ca6d520aa70d2df81df36fc74e30c802a9
dpwynne <dpwynne@gmail.com>
2023-03-08 10:42:48 -0800

    forgot to re-initialize pos

--- src/BinCount.cpp
+++ src/BinCount.cpp
@@ -4,7 +4,7 @@ using namespace Rcpp;
 IntegerVector Cpp_BinCount_sorted(NumericVector x, NumericVector s) {
     IntegerVector counts(s.length() - 1);
     for (R_xlen_t binnum = 0; binnum < s.length(); binnum++){
-        for (R_xlen_t pos; pos < x.length(); pos++){
+        for (R_xlen_t pos = 0; pos < x.length(); pos++){
             if (x[pos] >= s[binnum + 1]) break;
             counts[binnum]++;
         }
