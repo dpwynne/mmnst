adc27e167ed35e6e1a027a2e1f5d314ec9b448a7
rramezan <ramezanic@gmail.com>
2025-02-06 14:55:16 -0500

    updated the funciton to be right-continuous everywhere, including at the two end-points of the terminal.points.

--- R/CtIndividualPoint.R
+++ R/CtIndividualPoint.R
@@ -1,9 +1,9 @@
 #' \eqn{c(t)} at Individual Points
 #'
-#' Computes the estimated piecewise constant intensity function c(t) at a particular time point t1.
+#' Computes the estimated piecewise constant intensity function c(t) at a particular time point t1. The function is defined to be right-continuous.
 #'
 #' @param t1 a single time value.
-#' @param terminal.points a numeric vector containing the time points at which c(t) changes.
+#' @param terminal.points a numeric vector containing the time points at which c(t) changes. This vector is usually of length \eqn{2^J+1}.
 #' @param ct a numeric vector containing the estimated piecewise constant intensity function c(t).
 #'
 #' @return A single numeric value of c(t).
@@ -12,11 +12,10 @@
 
 CtIndividualPoint<-function(t1, terminal.points, ct){
 	##if t1 is outside the time frame specified by t.start and t.end, c(t1) is 0
-	if( t1 < min(terminal.points) | t1 > max(terminal.points) ) return(0)
-	##if t1 is t.start or t.end, c(t1) is the appropriate value
-	if (t1 == min(terminal.points)) return( ct[1] )
-	if (t1 == max(terminal.points)) return( ct[length(ct)] )
+	if( t1 <= min(terminal.points) | t1 > max(terminal.points) ) return(0)
 	##otherwise find the terminal point immediately before t1
-	ct.indx <- max(which(terminal.points <= t1))
+	ct.indx <- which(terminal.points[-length(terminal.points)] <= t1  & terminal.points[-1] > t1)
 	return(ct[ct.indx])
 }
+
+
